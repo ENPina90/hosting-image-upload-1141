014560d5ecfffb8e6a50bae6cf86083457846024
enpina90 <en.pina1@gmail.com>
2023-02-24 10:46:23 +0100

    image upload

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+require 'faker'
+
+puts "destroying articles"
+Article.all.each do |article|
+  article.photo.purge
+end
+Article.destroy_all
+
+file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
+article = Article.new(title: "NES", body: "A great console")
+article.photo.attach(io: file, filename: article.title, content_type: "image/png")
+article.save
+
+10.times do
+  file = URI.open('https://source.unsplash.com/featured?kitty')
+
+  article = Article.create(title: Faker::Hacker.say_something_smart, body: Faker::Hacker.say_something_smart)
+  article.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+  article.save
+  p article
+end
